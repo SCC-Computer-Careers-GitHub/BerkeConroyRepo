b9f6247431c26358ccae9fd2251d6bedf8675d71
Ethan Berke <eb.develops@gmail.com>
2022-03-29 12:33:22 -0500

    first iteration of calculator

--- ethan_berke.js
+++ ethan_berke.js
@@ -3,4 +3,4 @@ const bigNum = 42;
 const smallNum = 12;
 console.log("the numbers are: " + bigNum + " and " + smallNum);
 const numDiff = bigNum - smallNum;
-console.log("difference is: " + numDif);
+console.log("difference is: " + "numDiff");
