67abd601a9fdb91ad63b7c577fef728494201945
Ethan Berke <eb.develops@gmail.com>
2022-03-29 12:23:45 -0500

    first iteration of calculator

---
+++ ethan_berke.js
@@ -0,0 +1,6 @@
+//Subtract 2 numbers
+const bigNum = 42;
+const smallNum = 12;
+console.log("the numbers are: " + bigNum + " and " + smallNum);
+const numDiff = bigNum - smallNum;
+console.log("difference is: " + numDif);
